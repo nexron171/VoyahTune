0d79e3dfcad6b54c563f5de6896bbe3122f9cf5c
Sergey <nexron171@gmail.com>
2026-09-22 18:56:31 +0300

    release: prepare VoyahTune 3.11.1

--- Native/app/build.gradle.kts
+++ Native/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.big.town.anative"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "3.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- RestoreMode/app/build.gradle.kts
+++ RestoreMode/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.big.town.restoremode"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "3.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
